f3b92ce2dfc82bcb7a7abc53d0f40f48b69466cf
Mehdi Feki <mehdi.feki@palpix.com>
2017-10-08 15:25:04 +0200

    First commit

---
+++ app/src/main/java/com/mfekim/testapinytimes/NYTArticleListActivity.java
@@ -0,0 +1,13 @@
+package com.mfekim.testapinytimes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NYTArticleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nyt_activity_main);
+    }
+}
